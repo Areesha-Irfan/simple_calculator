35ff4689f1397d9b719cabda0b0c5d11bb772804
Areesha-Irfan <163674228+Areesha-Irfan@users.noreply.github.com>
2024-05-17 10:57:08 -0700

    clicalculator

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answers = await inquirer.prompt([
+  { name: "firstNumber", type: "number", message: "Enter first number" },
+  { name: "secondNumber", type: "number", message: "Enter second number" },
+  {
+    message: " Kindly select one of the operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition",  "Subtraction",  "Multiplication", "Division"],
+  },
+]);
+
+if (answers.operator === "Addition") {
+  console.log(answers.firstNumber + answers.secondNumber);
+} else if (answers.operator === "Subtraction") {
+  console.log(answers.firstNumber - answers.secondNumber);
+} else if (answers.operator === "Multiplication") {
+  console.log(answers.firstNumber * answers.secondNumber);
+} else if (answers.operator === "Division") {
+  console.log(answers.firstNumber / answers.secondNumber);
+} else {
+  console.log("Please enter a valid number ! ");
+}
